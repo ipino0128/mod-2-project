7c54696c7f62faccd0f022b28f95dc8fcec80572
github username <github email address>
2018-10-04 10:30:37 -0400

    no more rspecs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.valid?
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def notes
